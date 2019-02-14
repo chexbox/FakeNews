f929f92f4414c1947538d27e21f9696225de0106
chexbox <ethan.g.s@outlook.com>
2019-02-14 13:31:42 -0600

    Update fakenews.user.js

--- fakenews.user.js
+++ fakenews.user.js
@@ -7,6 +7,10 @@
 // @match        *.huffingtonpost.com/*
 // @match        https://*.cnn.com/*
 // @match        *.msnbc.com/*
+// @match        *.cnbc.com/*
+// @match        *.nbc.com/*
+// @match        *.buzzfeed.com/*
+// @match        *.buzzfeednews.com/*
 // @grant        none
 // @updateURL    https://chexbox.github.io/FakeNews/fakenews.user.js
 // ==/UserScript==
@@ -22,7 +26,7 @@
     }
     var css = document.createElement("style");
     css.type = "text/css";
-    css.innerHTML = "body, div, section { background-image: url(https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fi.imgur.com%2FLMgs3xU.gif%3Fnoredirect&f=1), url(https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fmedia.indiatimes.in%2Fmedia%2Ffacebook%2F2017%2FJan%2Fgghju_1484224447_1484224456_800x420.jpg&f=1); }";
+    css.innerHTML = "body, div, section { background-size: contain; background-image: url(https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fi.imgur.com%2FLMgs3xU.gif%3Fnoredirect&f=1), url(https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fmedia.indiatimes.in%2Fmedia%2Ffacebook%2F2017%2FJan%2Fgghju_1484224447_1484224456_800x420.jpg&f=1); }";
     document.body.appendChild(css);
     //unsafeWindow.onload = '(function(){for (let i; i < document.getElementsByTagName("img").length; i++){document.getElementsByTagName("img")[i].src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Famericafirstpatriots.com%2Fwp-content%2Fuploads%2F2017%2F06%2Fvery-fake-news-trump.jpg&f=1";}})();';
 })();
